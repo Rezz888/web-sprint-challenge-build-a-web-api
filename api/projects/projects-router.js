4a9e439eb2eacedb5fbc26473988800881979fdc
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-02-21 02:47:18 -0500

    Done performing CRUD operations in projects forlder

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,103 @@
 // Write your "projects" router here!
+
+const express = require("express")
+const projects = require("./projects-model")
+
+const router = express.Router()
+
+router.get("/api/projects", (req, res)=> {
+    projects.get()
+    .then((project)=> {
+        res.json(project)
+
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.status(500).json({
+            message: "There was an error"
+        })
+    })
+})
+
+router.get("/api/projects/:id", (req, res)=> {
+    projects.get(req.params.id)
+    .then((project)=> {
+        if (project){
+            res.json(project)
+        } else {
+            res.status(404).json({
+                message: "The project couldn't be found"
+            })
+        }
+
+        })
+        .catch((err)=> {
+           console.log(err)
+           res.status(500).json({
+              message: "There was an error"
+           })
+        })
+    })
+
+    router.post("/api/projects", (req, res)=> {
+        if (!req.body.name || !req.body.description){
+           return res.status(400).json({
+                message: "missing name or description"
+            })
+        }
+        
+        projects.insert(req.body)
+        .then((project)=> {
+            res.status(201).json(project)
+        })
+        .catch((err)=> {
+            console.log(err)
+            res.status(500).json({
+              message: "There was an error"
+            })
+        })
+    })
+
+ 
+router.put("/api/projects/:id", (req, res)=> {
+    projects.update(req.params.id, req.body)
+     .then((project)=> {
+        if(project){
+            res.json(project)
+        } else {
+            res.status(404).json({
+                message: "The project does not exist"
+            })
+        }
+     })
+     .catch((err)=> {
+        console.log(err)
+        res.status(500).json({
+            message: "there was an error"
+        })
+    })
+})
+
+router.delete("/api/projects/:id", (req, res)=> {
+    projects.remove(req.params.id)
+     .then((count)=> {
+        if (count > 0){
+            res.json({
+                message: "The project has been nuked"
+            })
+        } else {
+            res.status(404).json({
+                message: "The project could not be found",
+            })
+        }
+     })
+     .catch((err)=> {
+        console.log(err)
+        res.status(500).json({
+            message: "there was an error"
+        })
+     })
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const server = express();
 const actionsRouter = require("./actions/actions-router")
+const projectsRouter = require("./projects/projects-router")
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 server.use(express.json())
 server.use(actionsRouter)
+server.use(projectsRouter)
 
 
 module.exports = server;
